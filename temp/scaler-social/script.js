7b7007b5eef2ffcf7c7154ad5ba638c2281479a9
Ishan Avasthi <87573177+ishanavasthi@users.noreply.github.com>
2024-02-15 18:27:58 +0530

    Update script.js

--- temp/scaler-social/script.js
+++ temp/scaler-social/script.js
@@ -9,16 +9,3 @@ tweetForm.addEventListener("submit", function (event) {
   tweetList.appendChild(tweet);
   tweetInput.value = "";
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const tweetForm = document.getElementById("tweetForm");
-  const commentsButton = document.querySelector(".comments");
-
-  commentsButton.addEventListener("click", function () {
-    if (tweetForm.style.display === "none") {
-      tweetForm.style.display = "block";
-    } else {
-      tweetForm.style.display = "none";
-    }
-  });
-});
